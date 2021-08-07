fb7e04aaea19b52a2a25b4af1cd2d536a6f07b4a
mamkin-itshnik <mamkin.itshnik@gmail.com>
2021-08-07 17:55:28 +0300

    make first request

--- weatherApp/ViewController.swift
+++ weatherApp/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchBar.delegate = self
     }
 
 
 }
 
+extension ViewController: UISearchBarDelegate{
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        guard let searchPlace = searchBar.text else{
+            return
+        }
+        let urlString = "http://api.weatherstack.com/current?access_key=1dd0154d53f7e880f24fedc47fdd31cb&query=\(searchPlace)"
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url){
+            (data,response,error) in
+            
+            do{
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers )
+                
+                
+            }catch let jsonError{
+                print(jsonError)
+            }
+        }
+        
+        
+    }
+}
